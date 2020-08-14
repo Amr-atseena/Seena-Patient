7a297f001252be442f31c705e11ad5417ff414ef
Ahmed Zaki <a.zaki.iosdev@gmail.com>
2020-08-14 14:16:26 +0200

    Initial Commit

---
+++ Seena Patient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Seena Patient
+//
+//  Created by RKAnjel on 8/14/20.
+//  Copyright © 2020 RKAnjel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
