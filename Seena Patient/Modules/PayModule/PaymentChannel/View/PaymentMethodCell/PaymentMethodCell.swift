20d14bfd2fb2e81b69ccc9b8c2cc6d94d03cff5f
Ahmed Zaki <a.zaki.iosdev@gmail.com>
2020-09-07 22:51:25 +0200

    feat(paymentMethods): init UI

---
+++ Seena Patient/Modules/PayModule/PaymentChannel/View/PaymentMethodCell/PaymentMethodCell.swift
@@ -0,0 +1,21 @@
+//
+//  PaymentMethodCell.swift
+//  Seena Patient
+//
+//  Created by Ahmed Zaki on 07/09/2020.
+//  Copyright © 2020 RKAnjel. All rights reserved.
+//
+
+import UIKit
+
+class PaymentMethodCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
