cdd283d3107e4d13e5bb928f63a46f5f1112eb2f
Ahmed Zaki <a.zaki.iosdev@gmail.com>
2020-09-12 21:25:47 +0200

    feat(entities): add user , status and loginResponse model

---
+++ Seena Patient/Entities/User.swift
@@ -0,0 +1,25 @@
+//
+//  User.swift
+//  Seena Patient
+//
+//  Created by Ahmed Zaki on 12/09/2020.
+//  Copyright © 2020 RKAnjel. All rights reserved.
+//
+
+import Foundation
+// MARK: - User
+struct User: Codable {
+    let userId: Int
+    let firstName, lastName: String
+    let image: String
+    let phone, birthDate, role: String
+
+    enum CodingKeys: String, CodingKey {
+        case userId = "id"
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case image, phone
+        case birthDate = "birth_date"
+        case role
+    }
+}
